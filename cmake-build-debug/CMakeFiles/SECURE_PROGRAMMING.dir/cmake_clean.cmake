011be422ee28c5636b1dfa569536f0626047530f
svetozar <svetozar@live.fr>
2019-06-17 15:01:06 +0200

    client serveur

---
+++ cmake-build-debug/CMakeFiles/SECURE_PROGRAMMING.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SECURE_PROGRAMMING.dir/main.c.o"
+  "SECURE_PROGRAMMING.pdb"
+  "SECURE_PROGRAMMING"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SECURE_PROGRAMMING.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
