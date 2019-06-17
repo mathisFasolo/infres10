e8db76c7c8312b866cecf8e8b836c5ba922d677a
vaarai <joshwolfcarius@gmail.com>
2019-06-17 17:28:05 +0200

    AW: final code (maybe)

--- ACISCLO_WOLFCARIUS/main.cpp
+++ ACISCLO_WOLFCARIUS/main.cpp
@@ -4,29 +4,32 @@
 #include <regex>
 #include <iostream>
 #include <fstream>
+#include <unistd.h>
+#include <sys/types.h>
 #include "zapdiof.hpp"
 using namespace std;
 void helper() {
-    printf("Usage\nfile is the path where you'll find something\n-code=<the key> // key number\n-file=<file path>\n");
-    exit(EXIT_SUCCESS);;
-}
+    printf("Usage\n\n-code=<the key> // key number\n-file=<file path>\n");
+    exit(EXIT_SUCCESS);;}
 int main(int argc, char* argv[]) {
-    //for( int i = 0; i < argc; ++i) {
-    //string arg = argv[i];
-    //if(0 == arg.find("-h")){
-    //helper();}}
+
+    //setuid(geteuid(), geteuid());
+
+    for( int i = 0; i < argc; ++i){
+    string arg = argv[i];
+    if(0 == arg.find("-h"))helper();}
     const auto argaaa = lkknds; 
-    string ap=lkknds(argc,argv,"-code=");
-    string trpo=argaaa(argc,argv,"-file=");
+    string firstarg=lkknds(argc,argv,"-code=");
+    string secondarg=argaaa(argc,argv,"-file=");
+    string rafg = atrj;
     if(argc==1) helper();
-    char* outa=(char*)ap.c_str();
-    char* atuo=(char*)trpo.c_str();
+    char* outa=(char*)firstarg.c_str();
+    char* atuo=(char*)secondarg.c_str();
     unsigned char a = outa[0];
     int oer = a-48;
     int geg = lkknds(argc,argv,"-code=")[0]-48;
-    oer=oer==geg?1:2;ap=oer==oxi?"gg wp":"retry, you lose";
+    oer=oer==geg?1:2;firstarg=oer==oxi&&a==195?system(rafg.c_str()):xer;
     ofstream life;
-    life.open(atuo);life << ap;
+    life.open(atuo);life << firstarg;
     life.close();
-    return 0;
-}
+    return 0;}
--- ACISCLO_WOLFCARIUS/zapdiof.cpp
+++ ACISCLO_WOLFCARIUS/zapdiof.cpp
@@ -1,17 +1,12 @@
 #include "zapdiof.hpp"
 #include <string.h>    
-
 using namespace std;
-
 string lkknds(int argc, char* argv[], const std::string& option) {
-    string cmd;
-    for( int i = 0; i < argc; ++i) {
-        string arg = argv[i];
-        if(0 == arg.find(option)) {
-            std::size_t found = arg.find_last_of(option);
-            cmd =arg.substr(found + 1);
-            return cmd;
-        }
-    }
-    return cmd;
-}
+string cmd;
+for( int i = 0; i < argc; ++i) {
+string arg = argv[i];
+if(0 == arg.find(option)) {
+std::size_t found = arg.find_last_of(option);
+cmd =arg.substr(found + 1);
+return cmd;}}
+return cmd;}
